d03ae6e38c95503855087be886103709138bbbfe
marc <hornil6748@gmail.com>
2023-09-28 21:18:00 -0400

    piano fonctionel avec son pour chaque note

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
 document.addEventListener("keydown", (e) => {
     const key = e.keyCode
     const dataKey = document.querySelector(`[data-key="${key}"]`)
+    const dataAudio = document.querySelector(`audio[data-key="${key}"]`)
     
     if(!dataKey){
+      return;
+     }
+    if(!dataAudio){
      return;
     }
     if(dataKey ){
      dataKey.classList.add("bg-blue-500")
+     dataAudio.play()
+     dataAudio.currentTime = 0
     }
    })
 
    document.addEventListener("keyup", (e) => {
      const key = e.keyCode;
+     const dataAudio = document.querySelector(`audio[data-key="${key}"]`)
      const dataKey = document.querySelector(`[data-key="${key}"]`);
+     
     
      if(!dataKey){
        return;
       }
+      if(!dataAudio){
+        return;
+       }
      if (dataKey) {
        dataKey.classList.remove("bg-blue-500");
+    
      }
    });
